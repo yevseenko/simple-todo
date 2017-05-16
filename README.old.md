86f1e52ddf9770a4a9dbad8a80faf29cb9e917c4
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2017-05-16 20:01:45 +0300

    Changes

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# simple-todo
-Simple todolist with Firebase
---
+++ src/data/database.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDZTIhnWrdBI6Hg5XiFrSct0QkJOr8Yp7g",
+  authDomain: "todolist-base.firebaseapp.com",
+  databaseURL: "https://todolist-base.firebaseio.com",
+  projectId: "todolist-base",
+  storageBucket: "todolist-base.appspot.com",
+  messagingSenderId: "972391637296"
+};
+
+firebase.initializeApp(config);
+
+const data = firebase.database();
+
+export default data;
