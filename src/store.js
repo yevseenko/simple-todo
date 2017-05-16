016c0f50338cf7ce152a0fa58d03e0b74c824d6b
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2017-05-16 20:27:04 +0300

    store

--- src/store.js
+++ src/store.js
@@ -1,2 +1,31 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import data from './data/database';
+
+const appReducer = combineReducers({
+  todos: todosReducer
+});
+
+function dataLoadingMiddleware({ getState, dispatch }) {
+  return (next) => (action) => {
+    if (action.type === 'LOAD_DATA') {
+      console.log('call load data');
+      loadData().then(data => {
+        console.log('call receive data');
+        dispatch({ type: 'RECEIVE_DATA', payload: data });
+      });
+    }
+    return next(action);
+  }
+}
+const store = createStore(
+  appReducer,
+  applyMiddleware(dataLoadingMiddleware)
+);
+
+function loadData() {
+  return new Promise((resolve, reject) => {
+    () => resolve(data);
+  });
+}
+
+export default store;
