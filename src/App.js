188bcf459c40d09dd97bb48c8bf822001d55fc25
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2017-06-02 18:08:10 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import { TodoForm, TodoList, Header } from './components'
 import { mapActionsToProps } from './actions/map_actions'
 import './stylesheets/App.css';
 
+import firebase from 'firebase';
+
 class App extends PureComponent {
   render() {
+    var provider = new firebase.auth.GoogleAuthProvider();
     return (
       <div className="App">
         <Header />
@@ -19,6 +22,27 @@ class App extends PureComponent {
             removeTodo={(id) => this.props.removeTodo(id)}
             toggleTodo={(id) => this.props.toggleTodo(id)}
             setFilter={(filter) => this.props.setFilter(filter)} />
+          <div><button onClick={() => {
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              var token = result.credential.accessToken;
+              console.log(token)
+              // The signed-in user info.
+              var user = result.user;
+              console.log(user)
+              // ...
+            }).catch(function (error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              console.log(email)
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+              // ...
+            });
+          }}>Google Login</button></div>
         </div>
       </div>
     );
